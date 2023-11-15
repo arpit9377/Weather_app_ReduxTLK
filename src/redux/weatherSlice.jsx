614af045aa9a3f779802770a4fef626989cd7131
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-15 11:12:22 +0530

    Update weatherSlice.jsx
    City Name fix

--- src/redux/weatherSlice.jsx
+++ src/redux/weatherSlice.jsx
@@ -2,12 +2,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+
 const weatherSlice = createSlice({
   name: 'weather',
   initialState: {
     data: null,
     loading: false,
     error: null,
+    city: '', 
   },
   reducers: {
     fetchWeatherStart: (state) => {
@@ -17,6 +19,7 @@ const weatherSlice = createSlice({
       state.loading = false;
       state.data = action.payload;
       state.error = null;
+      state.city = action.payload.city_name; 
     },
     fetchWeatherFailure: (state, action) => {
       state.loading = false;
@@ -36,7 +39,7 @@ export const fetchWeatherData = (city) => async (dispatch) => {
   try {
     dispatch(fetchWeatherStart());
     const response = await axios.get(
-      `https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=d308159d1aca4ece9a846c78d087bc1f`
+      `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=d308159d1aca4ece9a846c78d087bc1f`
     );
     dispatch(fetchWeatherSuccess(response.data));
   } catch (error) {
